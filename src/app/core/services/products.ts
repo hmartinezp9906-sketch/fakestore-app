d02553d16fa64aa74652b10d2bbc8ae2c7073d0a
Hatzell <hmartinezp.9906@gmail.com>
2026-04-13 21:56:09 -0500

    UPDATE:  mejora experiencia UI login, product detail y product list

--- src/app/core/services/products.ts
+++ src/app/core/services/products.ts
@@ -8,15 +8,16 @@ export class Products {
   private http = inject(HttpClient);
   private readonly API_URL = 'https://fakestoreapi.com';
 
-  getProducts(): Observable<Product[]> {
+  getProducts(): Observable<Product[]>  {
     return this.http.get<Product[]>(`${this.API_URL}/products`);
-  }
+    }
 
-  getProductById(id: number): Observable<Product> {
+  getProductById(id: number): Observable<Product> 
+  {
     return this.http.get<Product>(`${this.API_URL}/products/${id}`);
   }
 
   getCategories(): Observable<string[]> {
     return this.http.get<string[]>(`${this.API_URL}/products/categories`);
-  }
+      }
 }
